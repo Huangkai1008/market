40ebae2c7515b7367a71e9c225ddfe3c83a125fc
huangkai <18778335525@163.com>
2019-08-14 18:36:30 +0800

    配置mysql

--- go.mod
+++ go.mod
@@ -5,8 +5,6 @@ go 1.12
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gin-gonic/gin v1.4.0
-	github.com/pkg/errors v0.8.1 // indirect
-	go.uber.org/atomic v1.4.0 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.10.0
+	github.com/jinzhu/gorm v1.9.10
+	github.com/kr/pretty v0.1.0 // indirect
 )
---
+++ models/models.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"market/pkg/setting"
+	"time"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	// 默认model结构体
+	ID         uint      `gorm:"primary_key" json:"id"`
+	CreateTime time.Time `json:"create_time"`
+	UpdateTime time.Time `json:"update_time"`
+}
+
+func init() {
+	var (
+		err                                  error
+		dbType, dbName, user, password, host string
+	)
+
+	dbType = setting.Type
+	dbName = setting.Name
+	user = setting.User
+	password = setting.Password
+	host = setting.Host
+	db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user,
+		password,
+		host,
+		dbName))
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.SingularTable(true) // 禁用复数表
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+}
