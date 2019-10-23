082c49f794bdd0f492493fa32ca07694c60c3b3a
huangkai <18778335525@163.com>
2019-10-23 17:03:08 +0800

    feat: 修改商品分类模型

--- models/product.go
+++ models/product.go
@@ -7,5 +7,6 @@ type ProductCategory struct {
 	CatName     string `gorm:"type:varchar(64);unique" json:"cat_name"`             // 分类名
 	CatLevel    uint8  `gorm:"type:tinyint(1);index:cat_level" json:"cat_level"`    // 分类等级, 0->1级; 1->2级
 	CatKeyWords string `gorm:"type:varchar(255)" json:"cat_key_words"`              // 分类关键词
+	CatIcon     string `gorm:"type:varchar(255)"`                                   // 分类图标
 	CatDesc     string `gorm:"type:text" json:"cat_desc"`                           // 分类描述
 }
