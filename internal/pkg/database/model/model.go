926122016089e9c538aef84d1acbbd5ef711d082
huangkai <18778335525@163.com>
2020-01-20 23:05:44 +0800

    feat: 基础模型主键结构修改

--- internal/pkg/database/model/model.go
+++ internal/pkg/database/model/model.go
@@ -4,7 +4,7 @@ import "market/internal/pkg/utils"
 
 // BaseModel 默认model结构体
 type BaseModel struct {
-	ID        uint           `gorm:"type:bigint(10);primary_key" json:"id"`
+	ID        uint           `gorm:"type:bigint(11) unsigned auto_increment;primary_key" json:"id"`
 	CreatedAt utils.JsonTime `gorm:"type:datetime;column:create_time;comment:'创建时间'" json:"create_time"`
 	UpdatedAt utils.JsonTime `gorm:"type:datetime;column:update_time;comment:'更新时间'" json:"update_time"`
 }
